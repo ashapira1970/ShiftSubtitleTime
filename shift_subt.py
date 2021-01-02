c56c370acd157746ff6b844200c529377712d578
Avi Shapira <shapshap@gmail.com>
2021-01-02 23:36:34 +0200

    fixed version

--- shift_subt.py
+++ shift_subt.py
@@ -1,35 +1,63 @@
 import re
 from datetime import datetime, timedelta
+import argparse
+from progress.bar import Bar
 
 
 
-def main():
-    delta = -18
-    file1 = open('C:\\Users\\ashap\\Downloads\\Dolittle.2020.HDRip.800MB.x264-GalaxyRG[TGx]\\Dolittle.2020.HDRip.800MB.x264-GalaxyRG.srt.orig', 'r', encoding='utf8')     
-    fout = open('C:\\Users\\ashap\\Downloads\\Dolittle.2020.HDRip.800MB.x264-GalaxyRG[TGx]\\Dolittle.2020.HDRip.800MB.x264-GalaxyRG.srt', 'wt', encoding='utf8')
-    Lines = file1.readlines() 
+def main(args):
+    source_path = args.sourcePath
+    target_path = args.targetPath
+    delta = args.delta        
+    fin = open(source_path, 'r', encoding='utf8')     
+    fout = open(target_path, 'wt', encoding='utf8')
+    lines = fin.readlines() 
     
-    count = 0
     FMT = '%H:%M:%S,%f'
+    print('starting to convet file...')
     # Strips the newline character 
-    for line in Lines:     
-        matchObj  = re.search( r'([0-9]*:[0-9]*:[0-9]*[0-9,]*) --> ([0-9]*:[0-9]*:[0-9]*[0-9,]*)', line, re.M|re.I)    
-        if matchObj:            
-            fromTimeStr = re.search( r'([0-9]*):([0-9]*):([0-9]*),([0-9]*)', matchObj.group(1), re.M|re.I)   
-            toTimeStr = re.search( r'([0-9]*):([0-9]*):([0-9]*),([0-9]*)', matchObj.group(2), re.M|re.I)   
-            
-            fromTime   = datetime.strptime(fromTimeStr.string, '%H:%M:%S,%f')
-            toTime   = datetime.strptime(toTimeStr.string, '%H:%M:%S,%f')
+    with Bar('Processing file', max=len(lines)) as bar:
+        for line in lines:     
+            matchObj  = re.search( r'([0-9]*:[0-9]*:[0-9]*[0-9,]*) --> ([0-9]*:[0-9]*:[0-9]*[0-9,]*)', line, re.M|re.I)    
+            if matchObj:            
+                fromTimeStr = re.search( r'([0-9]*):([0-9]*):([0-9]*),([0-9]*)', matchObj.group(1), re.M|re.I)   
+                toTimeStr = re.search( r'([0-9]*):([0-9]*):([0-9]*),([0-9]*)', matchObj.group(2), re.M|re.I)   
+                
+                fromTime   = datetime.strptime(fromTimeStr.string, FMT)
+                toTime   = datetime.strptime(toTimeStr.string, FMT)
 
-            fromTime = fromTime + timedelta(seconds=delta)
-            toTime =toTime + timedelta(seconds=delta)
-            line = line.replace(fromTimeStr.string, fromTime.strftime('%H:%M:%S,%f')[:-3])
-            line = line.replace(toTimeStr.string, toTime.strftime('%H:%M:%S,%f')[:-3])
-        fout.write(line)
-        #print(line)
+                fromTime = fromTime + timedelta(seconds=delta)
+                toTime =toTime + timedelta(seconds=delta)
+                line = line.replace(fromTimeStr.string, fromTime.strftime(FMT)[:-3])
+                line = line.replace(toTimeStr.string, toTime.strftime(FMT)[:-3])
+            fout.write(line)
+            bar.next()
+    print('finished converting file...')
 
 
 
 
 if __name__ == "__main__":
-    main()
+    """
+    example
+    python.exe .\shift_subt.py
+    --sourcePath 'C:\\Users\\ashap\\Downloads\\Dolittle.2020.HDRip.800MB.x264-GalaxyRG[TGx]\\Dolittle.2020.HDRip.800MB.x264-GalaxyRG.srt.orig' 
+    --targetPath 'C:\\Users\\ashap\\Downloads\\Dolittle.2020.HDRip.800MB.x264-GalaxyRG[TGx]\\Dolittle.2020.HDRip.800MB.x264-GalaxyRG.srt' 
+    --delta -18
+    """
+    
+    # Create the parser
+    my_parser = argparse.ArgumentParser(description='Shift subtitle time')
+    # Add the arguments
+    my_parser.add_argument('--sourcePath',
+                       type=str,
+                       help='the path to source file')
+    my_parser.add_argument('--targetPath',
+                       type=str,
+                       help='the path to output file')
+    my_parser.add_argument('--delta',
+                       type=int,
+                       help='Number of seconds to add/susbtract')
+    # Execute the parse_args() method
+    args = my_parser.parse_args()
+    main(args)
